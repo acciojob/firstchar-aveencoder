d513808099258521b9e55b1d884a2cbcd31e987d
aveencoder <aveen598@gmail.com>
2025-08-19 18:18:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function firstChar(text) {
- if (typeof text !== "string") {
-    return "";
-  }
+ 
 text = text.trim();
    if (text.length == 0) {
        return "";
@@ -11,5 +9,5 @@ text = text.trim();
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
